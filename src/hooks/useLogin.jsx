d0c6a7925f68cd72bc35ab82a3c6e67857e620dc
pen0914 <115807679+pen0914@users.noreply.github.com>
2023-03-31 21:53:56 +0900

    login=>navigate画面遷移

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,14 +1,18 @@
 import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSettingUser } from "./useSettingUser";
 
 export const useLogin = () => {
   const { setUserData } = useSettingUser();
+  const navigate = useNavigate();
   const data = { id: "1", pass: "1" };
   const login = useCallback((user) => {
     if (data.id === user.id) {
       if (data.pass === user.pass) {
         const selectData = data.id;
         setUserData(selectData);
+        alert("ログインしました。");
+        navigate("/main");
       } else {
         alert("パスワードが一致しません。");
       }
