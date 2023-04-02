d5905cabe748f69ba35cfbb85dc299ecc169362f
pen0914 <115807679+pen0914@users.noreply.github.com>
2023-04-02 16:20:29 +0900

    背景色の継続(Main)

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+import { useBgColor } from "../../hooks/useBgColor";
 
 export const Main = () => {
-  return <div>Homeページです</div>;
+  const { bgColor } = useBgColor();
+  return (
+    <SDiv bgColor={bgColor}>Homeページです</SDiv>
+  );
 };
+
+const SDiv = styled.div`
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  background-color: #${(props) => props.bgColor};
+`;
