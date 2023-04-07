711244b8cdd0ca571684cc3c66858407a442622e
pen0914 <115807679+pen0914@users.noreply.github.com>
2023-04-07 11:06:48 +0900

    main 数字の配置CSS

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -5,7 +5,29 @@ import { useBgColor } from "../../hooks/useBgColor";
 export const Main = () => {
   const { bgColor } = useBgColor();
   return (
-    <SDiv bgColor={bgColor}>Homeページです</SDiv>
+    <SDiv bgColor={bgColor}>
+      <div>
+        <button>□</button>
+        <button>□</button>
+        <button>□</button>
+      </div>
+      <SUnder>
+        <SNumberFlame>
+          <SNumber>0</SNumber>
+          <SNumber>1</SNumber>
+          <SNumber>2</SNumber>
+          <SNumber>3</SNumber>
+          <SNumber>4</SNumber>
+          <SNumber>5</SNumber>
+          <SNumber>6</SNumber>
+          <SNumber>7</SNumber>
+          <SNumber>8</SNumber>
+          <SNumber>9</SNumber>
+          <SNumber></SNumber>
+          <SNumber>×</SNumber>
+        </SNumberFlame>
+      </SUnder>
+    </SDiv>
   );
 };
 
@@ -15,4 +37,22 @@ const SDiv = styled.div`
   padding: 0;
   height: 100vh;
   background-color: #${(props) => props.bgColor};
+`;
+
+const SUnder = styled.div`
+  display: flex;
+  width: 50%;
+  height: 50%;
+`;
+
+const SNumberFlame = styled.div`
+  display: grid;
+  grid-template-columns: repeat(
+    3,
+    minmax(0, 1fr)
+  );
+`;
+
+const SNumber = styled.button`
+  display: grid;
 `;
